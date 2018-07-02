e86eb35624bd616bcc7e15590b9a5587ca057b06
Ashok Mohanakumar <ashok.mohank@gmail.com>
2018-07-02 20:29:01 +0530

    Add header

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+    <title>Otaku Penguin</title>
+</head>
+<body class="bg-light">
+    <div class="container-fluid px-0">
+        <nav class="navbar-dark bg-info px-lg-5 px-md-3">
+            <div class="navbar-brand">OP</div>
+        </nav>
+    </div>
+</body>
+</html>
---
+++ readme.md
@@ -0,0 +1,2 @@
+Simple PHP Social Network
+=========================
